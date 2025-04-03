ca074c03dcace76ad03afa9791b660f3aa1f1157
Eirik <eirik.niemi@artsdatabanken.no>
2025-04-03 08:28:05 +0200

    Add assessment sections #1489

--- Assessments.Web/Models/NatureTypes/NatureTypesDetailViewModel.cs
+++ Assessments.Web/Models/NatureTypes/NatureTypesDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Assessments.Data.Models;
 using Assessments.Shared.Constants;
+using Assessments.Web.Infrastructure;
 using Assessments.Web.Infrastructure.Enums;
 using RodlisteNaturtyper.Core.Models;
 using RodlisteNaturtyper.Data.Models;
@@ -8,6 +9,8 @@ namespace Assessments.Web.Models.NatureTypes;
 
 public record NatureTypesDetailViewModel(Assessment Assessment)
 {
+    public List<Region> Regions { get; set; } = [];
+
     public FeedbackViewModel FeedbackViewModel => new()
     {
         AssessmentId = Assessment.Id,
@@ -21,11 +24,63 @@ public record NatureTypesDetailViewModel(Assessment Assessment)
     {
         PageMenuContentId = NatureTypesConstants.PageMenuContentId,
         PageMenuExpandButtonText = NatureTypesConstants.Title2025,
-        AssessmentType = AssessmentType.NatureTypes2025
+        AssessmentType = AssessmentType.NatureTypes2025,
+        TableOfContentsViewModel = new TableOfContentsViewModel
+        {
+            Contents =
+            [
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Description),
+                    Title = NatureTypesConstants.Headings.Description,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Summary),
+                    Title = NatureTypesConstants.Headings.Summary,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.CodeItems),
+                    Title = NatureTypesConstants.Headings.CodeItems,
+                    ShouldShow = Assessment.CodeItems.Count != 0
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.CriteriaInformation),
+                    Title = NatureTypesConstants.Headings.CriteriaInformation,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.AreaInformation),
+                    Title = NatureTypesConstants.Headings.AreaInformation,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Regions),
+                    Title = NatureTypesConstants.Headings.Regions,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.References),
+                    Title = NatureTypesConstants.Headings.References,
+                    ShouldShow = Assessment.References.Count != 0
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(Constants.HeadingsNo.Citation),
+                    Title = Constants.HeadingsNo.Citation,
+                    ShouldShow = true
+                }
+            ]
+        }
     };
-
-    public List<Region> Regions { get; set; } = [];
-
+    
     public CitationForAssessmentViewModel CitationForAssessmentViewModel { get; set; }
     
     public List<CodeItemViewModel> CodeItemViewModels { get; set; }
