6f0110ce2ff67f19fabd8dd0defaa04b9310bf32
Eirik <69149042+eirikn-artsdatabanken@users.noreply.github.com>
2025-04-11 13:27:40 +0200

    Add buildnumber

--- Assessments.Web/Controllers/Api/TestController.cs
+++ Assessments.Web/Controllers/Api/TestController.cs
@@ -2,21 +2,34 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using System.Reflection;
 using Assessments.Shared.Constants;
+using LazyCache;
 
 namespace Assessments.Web.Controllers.Api;
 
 [ApiController]
 [Route("api/[controller]")]
 [EnableCors(nameof(CorsConstants.AllowAny))]
-public class TestController(IWebHostEnvironment environment, IOptions<ApplicationOptions> options) : ControllerBase
+public class TestController(IWebHostEnvironment environment, IOptions<ApplicationOptions> options, IAppCache cache) : ControllerBase
 {
     [HttpGet]
-    public IActionResult Test()
+    public async Task<IActionResult> Test()
     {
-        var buildTime = System.IO.File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location);
+        var buildNumber = "Unknown";
 
-        return Ok($"Hello from {environment.EnvironmentName}, baseUrl: {options.Value.BaseUrl}, buildTime: {buildTime:F}");
+        buildNumber = await cache.GetOrAddAsync($"{nameof(TestController)}-{nameof(buildNumber)}", async () =>
+        {
+            var buildNumberPath = Path.Combine(environment.WebRootPath, "BuildNumber.txt");
+
+            if (!System.IO.File.Exists(buildNumberPath))
+                return buildNumber;
+
+            using var file = new StreamReader(buildNumberPath);
+            buildNumber = await file.ReadLineAsync();
+
+            return buildNumber;
+        });
+
+        return Ok($"Hello from {environment.EnvironmentName}, baseUrl: {options.Value.BaseUrl}, buildNumber: {buildNumber}");
     }
 }
