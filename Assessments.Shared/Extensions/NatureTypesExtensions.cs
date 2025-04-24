6e648b507d8748ae3696b3187a55ca86466876bd
Eirik <eirik.niemi@artsdatabanken.no>
2025-04-24 07:58:44 +0200

    Add criteriainformation #1516 (wip)

--- Assessments.Shared/Extensions/NatureTypesExtensions.cs
+++ Assessments.Shared/Extensions/NatureTypesExtensions.cs
@@ -22,4 +22,56 @@ public static class NatureTypesExtensions
         
         return citation;
     }
+
+    public static string GetDescription(this CriteriaCategory category, CriteriaCategoryType type)
+    {
+        return type switch
+        {
+            CriteriaCategoryType.A => category switch
+            {
+                CriteriaCategory.CR => "≥ 80% CR",
+                CriteriaCategory.EN => "≥ 50% EN",
+                CriteriaCategory.VU => "≥ 30% VU",
+                CriteriaCategory.NT => "≥ 20% NT",
+                CriteriaCategory.LC => nameof(CriteriaCategory.LC),
+                CriteriaCategory.DD => nameof(CriteriaCategory.DD),
+                CriteriaCategory.NE => nameof(CriteriaCategory.NE),
+                _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
+            },
+            CriteriaCategoryType.B1 => category switch
+            {
+                CriteriaCategory.CR => "≤ 2000 km2 CR",
+                CriteriaCategory.EN => "≤ 20 000 km2 EN",
+                CriteriaCategory.VU => "≤ 50 000 km2 VU",
+                CriteriaCategory.NT => "≤ 55 000 km2 NT",
+                CriteriaCategory.LC => nameof(CriteriaCategory.LC),
+                CriteriaCategory.DD => nameof(CriteriaCategory.DD),
+                CriteriaCategory.NE => nameof(CriteriaCategory.NE),
+                _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
+            },
+            CriteriaCategoryType.B2 => category switch
+            {
+                CriteriaCategory.CR => "≤ 2 CR",
+                CriteriaCategory.EN => "≤ 20 EN",
+                CriteriaCategory.VU => "≤ 50 VU",
+                CriteriaCategory.NT => "≤ 55 NT",
+                CriteriaCategory.LC => nameof(CriteriaCategory.LC),
+                CriteriaCategory.DD => nameof(CriteriaCategory.DD),
+                CriteriaCategory.NE => nameof(CriteriaCategory.NE),
+                _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
+            },
+            CriteriaCategoryType.C => category switch
+            {
+                CriteriaCategory.CR => "≥ 80 %",
+                CriteriaCategory.EN => "≥ 50 %",
+                CriteriaCategory.VU => "≥ 30 %",
+                CriteriaCategory.NT => "≥ 20 %",
+                CriteriaCategory.LC => "< 20 %",
+                CriteriaCategory.DD => nameof(CriteriaCategory.DD),
+                CriteriaCategory.NE => nameof(CriteriaCategory.NE),
+                _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
 }
