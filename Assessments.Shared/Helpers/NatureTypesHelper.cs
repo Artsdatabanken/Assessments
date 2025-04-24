1c5cb685a60e8fe9e31beb4fbf2eb805a9e8cca2
Eirik <69149042+eirikn-artsdatabanken@users.noreply.github.com>
2025-04-24 11:26:27 +0200

    Add categorycriteria types #1516 (#1540)

---
+++ Assessments.Shared/Helpers/NatureTypesHelper.cs
@@ -0,0 +1,15 @@
+﻿using Assessments.Shared.DTOs.NatureTypes.Enums;
+
+namespace Assessments.Shared.Helpers;
+
+public static class NatureTypesHelper
+{
+    public static List<CategoryCriteriaType> GetCategoryCriteriaTypes(string categoryCriteria)
+    {
+        if (string.IsNullOrEmpty(categoryCriteria))
+            return [];
+
+        // utslagsgivende kriterier fra "Endelig kategori og kriterium"
+        return [.. Array.ConvertAll(categoryCriteria[2..].Split('+'), x => x.Trim()[..1]).Distinct().ToEnumerable<CategoryCriteriaType>()];
+    }
+}
