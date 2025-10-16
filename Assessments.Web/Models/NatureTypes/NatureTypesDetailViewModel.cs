a9c7959288c07e6b6242a65ca7c97c632e44da10
Eirik <eirik.niemi@artsdatabanken.no>
2025-10-16 14:10:35 +0200

    Add table of contents (basic version for now)

--- Assessments.Web/Models/NatureTypes/NatureTypesDetailViewModel.cs
+++ Assessments.Web/Models/NatureTypes/NatureTypesDetailViewModel.cs
@@ -12,68 +12,63 @@ namespace Assessments.Web.Models.NatureTypes;
 public record NatureTypesDetailViewModel(Assessment Assessment)
 {
     public IEnumerable<CodeItemNodeDto> CodeItemNodeDtos { get; init; }
-    
-    //public PageMenuViewModel PageMenuViewModel => new()
-    //{
-    //    //PageMenuContentId = NatureTypesConstants.PageMenuContentId,
-    //    PageMenuExpandButtonText = NatureTypesConstants.Title2025,
-    //    AssessmentType = AssessmentType.NatureTypes2025,
-    //    TableOfContentsViewModel = new TableOfContentsViewModel
-    //    {
-    //        Contents =
-    //        [
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.Description),
-    //                Title = NatureTypesConstants.Headings.Description,
-    //                ShouldShow = !string.IsNullOrEmpty(Assessment.DescriptionHtml)
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.Summary),
-    //                Title = NatureTypesConstants.Headings.Summary,
-    //                ShouldShow = true
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.CodeItems),
-    //                Title = NatureTypesConstants.Headings.CodeItems,
-    //                ShouldShow = CodeItemNodeDtos.Any()
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.CriteriaInformation),
-    //                Title = NatureTypesConstants.Headings.CriteriaInformation,
-    //                ShouldShow = true
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.AreaInformation),
-    //                Title = NatureTypesConstants.Headings.AreaInformation,
-    //                ShouldShow = true
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.Regions),
-    //                Title = NatureTypesConstants.Headings.Regions,
-    //                ShouldShow = true
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(NatureTypesConstants.Headings.References),
-    //                Title = NatureTypesConstants.Headings.References,
-    //                ShouldShow = Assessment.References.Count != 0
-    //            },
-    //            new TableOfContentsViewModel.Content
-    //            {
-    //                ElementId = nameof(Constants.HeadingsNo.Citation),
-    //                Title = Constants.HeadingsNo.Citation,
-    //                ShouldShow = true
-    //            }
-    //        ]
-    //    }
-    //};
-    
+
+
+    public TableOfContentsViewModel TableOfContentsViewModel => new()
+    {
+        Contents =
+            [
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Description),
+                    Title = NatureTypesConstants.Headings.Description,
+                    ShouldShow = !string.IsNullOrEmpty(Assessment.DescriptionHtml)
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Summary),
+                    Title = NatureTypesConstants.Headings.Summary,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.CodeItems),
+                    Title = NatureTypesConstants.Headings.CodeItems,
+                    ShouldShow = CodeItemNodeDtos.Any()
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.CriteriaInformation),
+                    Title = NatureTypesConstants.Headings.CriteriaInformation,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.AreaInformation),
+                    Title = NatureTypesConstants.Headings.AreaInformation,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.Regions),
+                    Title = NatureTypesConstants.Headings.Regions,
+                    ShouldShow = true
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(NatureTypesConstants.Headings.References),
+                    Title = NatureTypesConstants.Headings.References,
+                    ShouldShow = Assessment.References.Count != 0
+                },
+                new TableOfContentsViewModel.Content
+                {
+                    ElementId = nameof(Constants.HeadingsNo.Citation),
+                    Title = Constants.HeadingsNo.Citation,
+                    ShouldShow = true
+                }
+            ]
+    };
+
     public CitationForAssessmentViewModel CitationForAssessmentViewModel { get; init; }
     
     public CategoryDescriptionViewModel CategoryDescriptionViewModel = new()
